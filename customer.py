c1699cc2598240102af60382cfff1b5a25ef4a49
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-26 22:30:14 -0400

    coded customer.py file

--- customer.py
+++ customer.py
@@ -1,4 +1,15 @@
+from shopping_cart import ShoppingCart
+
 class Customer:
 
-    def __init__(self):
-        self.
+    def __init__(self, name):
+        self.name = name
+        self.cart = ShoppingCart()
+
+    def add_to_cart(self, item):
+        self.cart.add_to_cart(item)
+        print(f"{self.name} has added {item} to the cart!")
+
+    def items_in_cart(self):
+        for item in self.cart.item:
+            print(item.name)
