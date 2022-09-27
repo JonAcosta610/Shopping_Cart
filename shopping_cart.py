3385d4be01e39a4a65aac8d1e0be70c05e3b00a5
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-26 21:27:47 -0400

    coded the shopping_cart.py file per assignment requests

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,4 +2,17 @@ class ShoppingCart:
 
     def __init__(self):
         self.cart = []
-        
+
+    def cart_total(self):
+        total = 0
+        for item in self.cart:
+            total += product.item_price
+        return total
+
+    def add_to_cart(self, product):
+        self.cart.append(product)
+        print(f"You have added {product.item_name}")
+
+    def empty_cart(self):
+        self.cart.clear()
+        print("Your cart is now empty!")
